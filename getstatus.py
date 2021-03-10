498e2f6c064d1d3608f850336bd50c0b7343c335
plowsof <77655812+plowsof@users.noreply.github.com>
2021-03-10 03:16:07 +0000

    Update getstatus.py
    now returns value (e.g. to get mapname from server)

--- getstatus.py
+++ getstatus.py
@@ -1,6 +1,6 @@
 import socket
 import time
-UDP_IP = "localhost"
+UDP_IP = "127.0.0.1"
 UDP_PORT = 28910
 UDP_RCON_PASSWORD = "$rcon_password"
 print("UDP target IP:", UDP_IP)
@@ -11,9 +11,8 @@ def udp_send(MESSAGE):
 	global UDP_PORT,UDP_IP,UDP_RCON_PASSWORD
 	sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM) # UDP
 	for x in MESSAGE:
-		print(x)
 		pak = f"\xFF\xFF\xFF\xFFrcon {UDP_RCON_PASSWORD} {x}"
 		sock.sendto(bytes(pak, "latin-1"), (UDP_IP, UDP_PORT))
 		data = sock.recv(1400)
-		print(data.decode('latin-1'))
-		time.sleep(0.3)
+		resp = data.decode('latin-1')
+		return resp.split("\n")[1]
